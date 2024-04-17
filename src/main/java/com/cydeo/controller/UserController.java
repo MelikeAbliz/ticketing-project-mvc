448944771fdf33bb47db4c3852941c1d55d1263f
MelikeAbliz <medine20171121@gmail.com>
2024-04-17 12:39:03 +0300

    ticketing-project-part2

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -2,6 +2,7 @@ package com.cydeo.controller;
 
 import com.cydeo.dto.UserDTO;
 import com.cydeo.service.RoleService;
+import com.cydeo.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,16 +12,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/user")
 public class UserController {
     private final RoleService roleService;
+    private final UserService userService;
 
-    public UserController(RoleService roleService) {
+    public UserController(RoleService roleService, UserService userService) {
         this.roleService = roleService;
+        this.userService = userService;
     }
 
     @GetMapping("create")
     public String createForm(Model model){
         model.addAttribute("user",new UserDTO());
         model.addAttribute("roles",roleService.findAll());
-
+        model.addAttribute("users",userService.findAll());
         return "/user/create";
     }
 }
