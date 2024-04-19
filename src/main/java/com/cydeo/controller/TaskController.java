a60d128248677859e87984e703b385f539fdc34e
MelikeAbliz <medine20171121@gmail.com>
2024-04-19 15:03:51 +0300

    ticketing-project-part5 completed task

--- src/main/java/com/cydeo/controller/TaskController.java
+++ src/main/java/com/cydeo/controller/TaskController.java
@@ -1,16 +1,67 @@
 package com.cydeo.controller;
 
+import com.cydeo.dto.TaskDTO;
+import com.cydeo.service.ProjectService;
+import com.cydeo.service.TaskService;
+import com.cydeo.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/task")
 public class TaskController {
+    private final TaskService taskService;
+    private final ProjectService projectService;
+    private final UserService userService;
+
+    public TaskController(TaskService taskService, ProjectService projectService, UserService userService) {
+        this.taskService = taskService;
+        this.projectService = projectService;
+        this.userService = userService;
+    }
+
     @GetMapping("/create")
     public String createTask(Model model){
 
+        model.addAttribute("task",new TaskDTO());
+        model.addAttribute("projects",projectService.findAll());
+        model.addAttribute("employees",userService.findEmployees());
+        model.addAttribute("tasks",taskService.findAll());
         return "task/create";
     }
+    @PostMapping("/create")
+    public String saveTask(TaskDTO task){
+        taskService.save(task);
+
+        return "redirect:/task/create";
+    }
+    @GetMapping("/delete/{id}")
+    public String deleteTask(@PathVariable("id") Long id){
+        taskService.deleteById(id);
+        return "redirect:/task/create";
+    }
+    @GetMapping("/update/{taskId}")
+    public String editTask(@PathVariable("taskId") Long taskId,Model model){
+        model.addAttribute("task",taskService.findById(taskId));
+        model.addAttribute("projects",projectService.findAll());
+        model.addAttribute("employees",userService.findEmployees());
+        model.addAttribute("tasks",taskService.findAll());
+        return "/task/update";
+    }
+//    @PostMapping("/update/{taskId}")
+//    public String updateTask(@PathVariable("taskId") Long taskId, TaskDTO task){
+//        task.setId(taskId);
+//        taskService.update(task);
+//        return "redirect:/task/create";
+//    }
+@PostMapping("/update/{taskId}")
+public String updateTask(TaskDTO task){
+    taskService.update(task);
+    return "redirect:/task/create";
+}
+
 }
--- src/main/java/com/cydeo/service/impl/TaskServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/TaskServiceImpl.java
@@ -1,34 +1,50 @@
 package com.cydeo.service.impl;
 
 import com.cydeo.dto.TaskDTO;
-import com.cydeo.service.ProjectService;
+import com.cydeo.enums.Status;
 import com.cydeo.service.TaskService;
+import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.UUID;
 
+@Service
 public class TaskServiceImpl extends AbstractMapService<TaskDTO,Long> implements TaskService {
     @Override
-    public TaskDTO save(TaskDTO user) {
-        return null;
+    public TaskDTO save(TaskDTO task) {
+        if (task.getTaskStatus()==null)
+            task.setTaskStatus(Status.OPEN);
+        if (task.getAssignedDate()==null)
+            task.setAssignedDate(LocalDate.now());
+
+        if(task.getId()==null)
+            task.setId(UUID.randomUUID().getMostSignificantBits());
+        return super.save(task.getId(),task);
     }
 
     @Override
     public TaskDTO findById(Long id) {
-        return null;
+        return super.findById(id);
     }
 
     @Override
     public List<TaskDTO> findAll() {
-        return null;
+        return super.findAll();
     }
 
     @Override
-    public void update(TaskDTO object) {
-
+    public void update(TaskDTO task) {
+        if (task.getTaskStatus()==null)
+            task.setTaskStatus(Status.OPEN);
+        if (task.getAssignedDate()==null)
+            task.setAssignedDate(LocalDate.now());
+        super.update(task.getId(),task);
     }
 
     @Override
     public void deleteById(Long id) {
 
+        super.deleteById(id);
     }
 }
