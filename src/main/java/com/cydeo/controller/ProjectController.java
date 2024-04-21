f8551741ecc44bb83e3ea9a59810dab727a18cf3
MelikeAbliz <medine20171121@gmail.com>
2024-04-21 13:13:48 +0300

    ticketing-project-part5 completed

--- src/main/java/com/cydeo/controller/ProjectController.java
+++ src/main/java/com/cydeo/controller/ProjectController.java
@@ -6,8 +6,10 @@ import com.cydeo.service.ProjectService;
 import com.cydeo.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -35,8 +37,14 @@ public class ProjectController {
     }
 
     @PostMapping("/create")
-    public String insertProject(@ModelAttribute("project") ProjectDTO project) {
+    public String insertProject(@Valid @ModelAttribute("project") ProjectDTO project, BindingResult bindingResult,Model model) {
+        if (bindingResult.hasErrors()){
+            model.addAttribute("managers", userService.findManager());
 
+            model.addAttribute("projects", projectService.findAll());
+
+            return "/project/create";
+        }
 
         projectService.save(project);
 
@@ -70,23 +78,32 @@ public class ProjectController {
     }
 
     @PostMapping("/update")
-    public String updateProject(@ModelAttribute("project") ProjectDTO project) {
+    public String updateProject(@Valid @ModelAttribute("project") ProjectDTO project, BindingResult bindingResult,Model model) {
+        if (bindingResult.hasErrors()){
+            model.addAttribute("managers",userService.findManager());
+            model.addAttribute("projects",projectService.findAll());
 
-
+            return "/project/update";
+        }
         projectService.save(project);
 
         return "redirect:/project/create";
     }
-    @GetMapping("manager/project-status")
-    public String editProjectStatus( Model model){
+    @GetMapping("/manager/project-status")
+    public String getProjectByManager( Model model){
 
-        UserDTO manager = userService.findById("john@cydeo.com");
+        UserDTO manager = userService.findById("delisa@cydeo.com");
         List<ProjectDTO> projects = projectService.getCountedListOfProjectDTO(manager);
 
         model.addAttribute("projects", projects);
 
         return "/manager/project-status";
 
+    }
+    @GetMapping("/manager/complete/{projectCode}")
+    public String managerCompleteProject(@PathVariable("projectCode") String projectCode) {
+        projectService.complete(projectService.findById(projectCode));
+        return "redirect:/project/manager/project-status";
     }
 
 
--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -5,7 +5,10 @@ import com.cydeo.service.RoleService;
 import com.cydeo.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/user")
@@ -18,7 +21,7 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping("create")
+    @GetMapping("/create")
     public String createForm(Model model){
         model.addAttribute("user",new UserDTO());
         model.addAttribute("roles",roleService.findAll());
@@ -27,8 +30,13 @@ public class UserController {
     }
 
     @PostMapping("/create")
-    public String insertUser(@ModelAttribute("user") UserDTO user){
+    public String insertUser(@Valid @ModelAttribute("user") UserDTO user, BindingResult bindingResult,Model model){
 
+        if (bindingResult.hasErrors()){
+            model.addAttribute("roles",roleService.findAll());
+            model.addAttribute("users",userService.findAll());
+            return "/user/create";
+        }
         userService.save(user);
 
         return "redirect:/user/create";
@@ -45,7 +53,13 @@ public class UserController {
 
     }
     @PostMapping("/update")
-    public String updateUser(@ModelAttribute("user") UserDTO user){
+    public String updateUser(@Valid @ModelAttribute("user") UserDTO user,BindingResult bindingResult,Model model){
+        if (bindingResult.hasErrors()){
+            model.addAttribute("roles",roleService.findAll());
+            model.addAttribute("users",userService.findAll());
+            return "/user/update";
+        }
+
         userService.update(user);
 
         return "redirect:/user/create";
